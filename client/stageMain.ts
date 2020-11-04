49fa03ba831bae212364e51cf62e4a90f35b8e76
osaton <o@sploosh.org>
2020-11-04 09:04:36 +0200

    [WIP] Start stage server

---
+++ client/stageMain.ts
@@ -0,0 +1,107 @@
+import * as vscode from 'vscode';
+import { LanguageClient } from 'vscode-languageclient';
+import { generateGrammarCommandHandler } from './commands/generateGrammarCommand';
+import { registerLanguageConfigurations } from './languages';
+import { initializeLanguageClient } from './client';
+import { join } from 'path';
+import {
+  setVirtualContents,
+  registerVeturTextDocumentProviders,
+  generateShowVirtualFileCommand
+} from './commands/virtualFileCommand';
+import { getGlobalSnippetDir } from './userSnippetDir';
+import { generateOpenUserScaffoldSnippetFolderCommand } from './commands/openUserScaffoldSnippetFolderCommand';
+
+export async function activate(context: vscode.ExtensionContext) {
+  const isInsiders = vscode.env.appName.includes('Insiders');
+  const globalSnippetDir = getGlobalSnippetDir(isInsiders);
+
+  /**
+   * Virtual file display command for debugging template interpolation
+   */
+  context.subscriptions.push(await registerVeturTextDocumentProviders());
+
+  /**
+   * Custom Block Grammar generation command
+   */
+  context.subscriptions.push(
+    vscode.commands.registerCommand('vetur.generateGrammar', generateGrammarCommandHandler(context.extensionPath))
+  );
+
+  /**
+   * Open custom snippet folder
+   */
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      'vetur.openUserScaffoldSnippetFolder',
+      generateOpenUserScaffoldSnippetFolderCommand(globalSnippetDir)
+    )
+  );
+
+  registerLanguageConfigurations();
+
+  /**
+   * Vue Language Server Initialization
+   */
+
+  const serverModule = context.asAbsolutePath(join('stage-server', 'dist', 'serverMain.js'));
+  const client = initializeLanguageClient(serverModule, globalSnippetDir);
+  context.subscriptions.push(client.start());
+
+  const promise = client
+    .onReady()
+    .then(() => {
+      registerCustomClientNotificationHandlers(client);
+      registerCustomLSPCommands(context, client);
+      registerRestartVLSCommand(context, client);
+    })
+    .catch(e => {
+      console.log('Client initialization failed');
+    });
+
+  return displayInitProgress(promise);
+}
+
+async function displayInitProgress(promise: Promise<void>) {
+  return vscode.window.withProgress(
+    {
+      title: 'Vetur initialization',
+      location: vscode.ProgressLocation.Window
+    },
+    () => promise
+  );
+}
+
+function registerRestartVLSCommand(context: vscode.ExtensionContext, client: LanguageClient) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand('vetur.restartVLS', () =>
+      displayInitProgress(
+        client
+          .stop()
+          .then(() => client.start())
+          .then(() => client.onReady())
+      )
+    )
+  );
+}
+
+function registerCustomClientNotificationHandlers(client: LanguageClient) {
+  client.onNotification('$/displayInfo', (msg: string) => {
+    vscode.window.showInformationMessage(msg);
+  });
+  client.onNotification('$/displayWarning', (msg: string) => {
+    vscode.window.showWarningMessage(msg);
+  });
+  client.onNotification('$/displayError', (msg: string) => {
+    vscode.window.showErrorMessage(msg);
+  });
+  client.onNotification('$/showVirtualFile', (virtualFileSource: string, prettySourceMap: string) => {
+    setVirtualContents(virtualFileSource, prettySourceMap);
+  });
+}
+
+function registerCustomLSPCommands(context: vscode.ExtensionContext, client: LanguageClient) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand('vetur.showCorrespondingVirtualFile', generateShowVirtualFileCommand(client))
+  );
+}
